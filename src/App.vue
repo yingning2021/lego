44d3a688cb85531e8fd9153f6627e70903cad414
yingning2021 <yingning2021@163.com>
2022-04-24 22:44:05 +0800

    feat: create sketch

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,7 @@
 <template>
-  <h1>慕课乐高标准模版</h1>
-  <ul>
-    <li>开箱即用</li>
-    <li>typescript</li>
-    <li>Vue3</li>
-    <li>支持 tsx</li>
-    <li>eslint</li>
-    <li>简单易用可扩展</li>
-  </ul>
+  <div class="app-container">
+    <router-view/>
+  </div>
 </template>
 
 <script lang="ts">
@@ -19,12 +13,7 @@ export default defineComponent({
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.page-title {
+  color: #fff;
 }
 </style>
